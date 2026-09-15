4fb38423d0716341f7c04c13254d39fb3aad1570
Jean Saban <jeansaban@Jeans-MBP.modem>
2026-09-15 17:19:04 +0200

    security utilities for hashing password

--- src/core/security.py
+++ src/core/security.py
@@ -1,13 +1,20 @@
-"""Exception handler."""
+"""Security utilities."""
+
+from argon2 import PasswordHasher
+from argon2.exceptions import VerifyMismatchError
+
+_password_hasher = PasswordHasher()
 
 
-class NotFoundError(Exception):
-    """Représente une ressource qui n'a pas été trouvée."""
+def hash_password(password: str) -> str:
+    """Hash a plain text password."""
+    return _password_hasher.hash(password)
 
-    pass
-
-
-class ConflictError(Exception):
-    """Représente une ressource qui entre en conflit avec une ressource existante."""
-
-    pass
+def verify_password(password: str, hashed_password: str) -> bool:
+    """Verify a plain text password against a hashed password."""
+    try:
+        _password_hasher.verify(hashed_password, password)
+        return True
+    except VerifyMismatchError:
+        return False
+    
